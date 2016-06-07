160bb934ce179106b62d353d21f059d497f6c240
Chyi <Chyi@LEE0235>
2016-06-07 18:05:20 +0800

    加入Widget 10%實做

---
+++ JustBlog/Models/WidgetViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JustBlog.Core;
+using JustBlog.Core.Objects;
+
+namespace JustBlog.Models
+{
+    public class WidgetViewModel
+    {
+        public WidgetViewModel(IBlogRepository blogRepository)
+        {
+            Categories = blogRepository.Categories();
+        }
+
+        public IList<Category> Categories { get; private set; }
+    }
+}
