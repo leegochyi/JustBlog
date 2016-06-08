2108328a19e0bc6f4060a5264a10d6d42fd1e784
Chyi <Chyi@LEE0235>
2016-06-08 15:36:09 +0800

    新增LatestPosts  教學part1 漏掉在WidgetViewModel 補上資料取得方法

--- JustBlog/Models/WidgetViewModel.cs
+++ JustBlog/Models/WidgetViewModel.cs
@@ -14,10 +14,14 @@ namespace JustBlog.Models
             Categories = blogRepository.Categories();
 
             Tags = blogRepository.Tags();
+
+            LatestPosts = blogRepository.Posts(0, 10);
         }
 
         public IList<Category> Categories { get; private set; }
 
         public IList<Tag> Tags { get; private set; }
+
+        public IList<Post> LatestPosts { get; private set; }
     }
 }
